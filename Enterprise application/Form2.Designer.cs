60e73ddb122610c4fcd8feb2254438c3606c8e0d
Deshaninayanamini <87793827+Deshaninayanamini@users.noreply.github.com>
2023-05-01 14:51:19 +0530

    adding

--- Enterprise application/Form2.Designer.cs
+++ Enterprise application/Form2.Designer.cs
@@ -28,13 +28,24 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(922, 272);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(326, 182);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
             // 
             // Form2
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1166, 545);
+            this.ClientSize = new System.Drawing.Size(1802, 850);
+            this.Controls.Add(this.button1);
             this.Name = "Form2";
             this.Text = "Form2";
             this.ResumeLayout(false);
@@ -42,5 +53,7 @@
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button1;
     }
 }
